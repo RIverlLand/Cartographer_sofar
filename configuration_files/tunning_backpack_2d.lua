85ef229cc15c8f26d4d04b53c0cad5ca63cbb61e
riverland <douglasell91@gmail.com>
2020-10-08 14:19:59 +0200

    files progressing to do the 2D and 3D localization

--- configuration_files/tunning_backpack_2d.lua
+++ configuration_files/tunning_backpack_2d.lua
@@ -22,15 +22,15 @@ options = {
   tracking_frame = "base_link",
   published_frame = "base_link",
   odom_frame = "odom",
-  provide_odom_frame = false,
+  provide_odom_frame = true,
   publish_frame_projected_to_2d = false,
   use_odometry = false,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 0,
-  num_multi_echo_laser_scans = 0,
-  num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 2,
+  num_multi_echo_laser_scans = 1,
+  num_subdivisions_per_laser_scan = 10,
+  num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
@@ -42,15 +42,12 @@ options = {
   landmarks_sampling_ratio = 1.,
 }
 
-TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 160
+--POSE_GRAPH.optimize_every_n_nodes = 10
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 4
 
-MAP_BUILDER.use_trajectory_builder_3d = true
-MAP_BUILDER.num_background_threads = 7
-POSE_GRAPH.optimization_problem.huber_scale = 5e2
-POSE_GRAPH.optimize_every_n_nodes = 320
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
-POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
-POSE_GRAPH.constraint_builder.min_score = 0.62
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
+
+MAP_BUILDER.use_trajectory_builder_2d = true
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
 
 return options
